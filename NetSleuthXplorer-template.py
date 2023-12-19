371852644dccc586766cac69318d405501e9d801
Curtis Hartsell <78432078+curtis-hartsell-tm@users.noreply.github.com>
2023-12-19 14:29:34 -0600

    Update NetSleuthXplorer-template.py
    added functionality to also provide IP reputation score from MDTI

--- NetSleuthXplorer-template.py
+++ NetSleuthXplorer-template.py
@@ -45,7 +45,6 @@ def resolve_dns(ip, access_token):
                 if 'zpath.net' in hostname:
                     return False, "Contains zpath.net"
 
-            # If zpath.net is not found, return the most recent hostname
             most_recent_record = max(records, key=lambda r: r.get('lastSeenDateTime', ''))
             most_recent_hostname = most_recent_record.get('artifact', {}).get('id', 'Unknown hostname')
             return True, most_recent_hostname
@@ -55,12 +54,28 @@ def resolve_dns(ip, access_token):
     else:
         return False, f"DNS resolution failed with status code {response.status_code}"
 
+# Function to get host reputation using MDTI API
+def get_host_reputation(ip, access_token):
+    url = f'https://graph.microsoft.com/v1.0/security/threatIntelligence/hosts/{ip}/reputation'
+    headers = {'Authorization': 'Bearer ' + access_token}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            reputation_info = data.get('value', {})
+            reputation = reputation_info.get('reputation', 'Unknown')
+            return reputation
+        except json.JSONDecodeError:
+            return "Failed to parse JSON response"
+    else:
+        return f"Failed with status code {response.status_code}"
+
 # Function to calculate Z-score
 def calculate_z_score(dataframe, value_column, group_columns):
     df_grouped = dataframe.groupby(group_columns)[value_column].agg(['mean', 'std'])
     df_merged = dataframe.merge(df_grouped, left_on=group_columns, right_on=group_columns, how='left')
     
-    # Handling cases where standard deviation is zero
     df_merged['z_score'] = df_merged.apply(
         lambda row: ((row[value_column] - row['mean']) / row['std']) 
                     if row['std'] != 0 else np.nan, axis=1)
@@ -79,21 +94,25 @@ df_with_zscore = calculate_z_score(df, 'num_octets', ['src_ip_addr', 'dst_ip_add
 # Filter out rows with significant deviation (e.g., Z-score > 2) and non-NaN Z-scores
 significant_deviation = df_with_zscore[(np.abs(df_with_zscore['z_score']) > 2) & (~np.isnan(df_with_zscore['z_score']))].copy()
 
-# Get unique DST_IP_ADDR for DNS resolution to avoid redundant lookups
+# Get unique DST_IP_ADDR for DNS resolution and reputation lookup
 unique_dst_ip_addresses = significant_deviation['dst_ip_addr'].unique()
 
 # Get access token
 access_token = get_access_token(client_id, client_secret, tenant_id)
 
-# Perform DNS resolution with progress bar
+# Perform DNS resolution and IP reputation lookup with progress bar
 dns_results = {}
-for ip in tqdm(unique_dst_ip_addresses, desc="Resolving DNS", unit="IP"):
+reputation_results = {}
+for ip in tqdm(unique_dst_ip_addresses, desc="Resolving DNS and Fetching IP Reputations", unit="IP"):
     success, hostname = resolve_dns(ip, access_token)
     if success:
         dns_results[ip] = hostname
+        reputation = get_host_reputation(ip, access_token)
+        reputation_results[ip] = reputation
 
-# Map the DNS results back to the DataFrame
+# Map the DNS and reputation results back to the DataFrame
 significant_deviation['dst_ip_dns'] = significant_deviation['dst_ip_addr'].apply(dns_results.get)
+significant_deviation['ip_reputation'] = significant_deviation['dst_ip_addr'].apply(reputation_results.get)
 
 # Filter out rows with failed or unwanted DNS resolutions
 significant_deviation = significant_deviation[significant_deviation['dst_ip_dns'].notna()]
@@ -102,4 +121,4 @@ significant_deviation = significant_deviation[significant_deviation['dst_ip_dns'
 top_10_results = significant_deviation.nlargest(10, 'z_score')
 
 # Display the top 10 results
-print(top_10_results[['src_ip_addr', 'dst_ip_addr', 'num_octets', 'z_score', 'dst_ip_dns']])
+print(top_10_results[['src_ip_addr', 'dst_ip_addr', 'num_octets', 'z_score', 'dst_ip_dns', 'ip_reputation']])
